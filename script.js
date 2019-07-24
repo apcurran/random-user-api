e84d47fbc295c328aa09d55c5a79827feec90deb
apc <alexcurran.va@gmail.com>
2019-07-24 16:28:34 -0500

    Finished site layout with grid, and JS API Fetch

---
+++ script.js
@@ -0,0 +1,33 @@
+const randomUserModule = (() => {
+    const ul = document.querySelector(".users");
+    const apiUrl = "https://randomuser.me/api/?results=10";
+
+    function capitalizeFirstLetter(str) {
+        return str[0].toUpperCase() + str.slice(1);
+    }
+
+    async function displayUsers() {
+        const response = await fetch(apiUrl);
+        const jsonResponse = await response.json();
+        const authors = await jsonResponse.results;
+
+        for (const author of authors) {
+            const li = document.createElement("li");
+            const img = document.createElement("img");
+            const span = document.createElement("span");
+            const capfirstName = capitalizeFirstLetter(author.name.first);
+            const caplastName = capitalizeFirstLetter(author.name.last);
+
+            li.classList.add("user");
+            img.src = author.picture.large;
+            span.textContent = `${capfirstName} ${caplastName}`;
+
+            li.appendChild(img);
+            li.appendChild(span);
+            ul.appendChild(li);
+        }
+    }
+
+    displayUsers()
+        .catch((err) => console.error(err));
+})();
